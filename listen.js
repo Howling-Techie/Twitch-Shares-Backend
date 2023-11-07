48b51ab9662fd5662ee3ab9a1eb9769ce3f6a3ac
Howling Techie <howling.techie@gmail.com>
2023-11-07 12:54:58 +0000

    Changed portname to allow render deploy

--- listen.js
+++ listen.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 const {PORT = 9090} = process.env;
 
-app.listen(PORT, "localhost", function () {
+app.listen(PORT, "0.0.0.0", function () {
     console.log("listening at http://%s:%s", "localhost", PORT);
 }).on("error", (e) => {
     console.error(e.message);
